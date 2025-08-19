541f54f4468aead56f596a4967a048dc6dae3f35
Kanav Rohatgi <kanavr@gmail.com>
2025-08-19 15:38:55 +0530

    customize defaultSecurityFilterChain

---
+++ src/main/java/com/security/project/config/ProjectSecurityConfigurations.java
@@ -0,0 +1,28 @@
+package com.security.project.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfigurations { 
+
+    @Bean
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		// http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests
+                                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                                        .requestMatchers("/notices", "/contact", "/error").permitAll());
+                                        
+		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		return http.build();
+	}
+
+}
