de364a0ddff47ec50ff484197910352e02d0b1b2
Kanav Rohatgi <kanavr@gmail.com>
2025-08-19 15:06:58 +0530

    add @RestController

--- src/main/java/com/security/project/controller/Accountcontroller.java
+++ src/main/java/com/security/project/controller/Accountcontroller.java
@@ -1,7 +1,9 @@
 package com.security.project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Accountcontroller {
     @GetMapping("/myAccount")
     public String getAccountDetails() {
