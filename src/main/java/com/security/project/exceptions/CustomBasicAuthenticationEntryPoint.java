1057dfdb41633e21cb1dec9cc595aec29a20d8de
Kanav Rohatgi <kanavr@gmail.com>
2025-08-21 14:11:49 +0530

    set custom response body for the exception

--- src/main/java/com/security/project/exceptions/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/com/security/project/exceptions/CustomBasicAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.security.project.exceptions;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -16,9 +17,21 @@ public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryP
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException, ServletException {
-            // Set Response headers
+            // Populate dynamic values
+            LocalDateTime currentTimeStamp = LocalDateTime.now();
+            String message = (authException != null && authException.getMessage() != null) ? authException.getMessage()
+                : "Unauthorized";
+                String path = request.getRequestURI();
+            // Set Custom Response headers
             response.setHeader("error-reason", "authentication failed");
-            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+            // Set Custom Response Body
+            response.setContentType("application/json;charset=UTF-8");
+
+            String jsonResponse = String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}", 
+            currentTimeStamp, HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(), message, path);
+            response.getWriter().write(jsonResponse);
         }
 
 }
